f4caa029604bd0f5cbb0af40d1de9b8bfeac73e2
skitsanos <info@skistanos.com>
2015-02-23 15:24:01 +0200

    added renditions to VideoDetails

--- com.vzaar.api/Vzaar.cs
+++ com.vzaar.api/Vzaar.cs
@@ -187,6 +187,10 @@ namespace com.vzaar.api
                     title = (string) jo["title"],
                     description = (string) jo["description"]
                 };
+
+                var renditions = new List<VideoRendition>();
+                JsonConvert.PopulateObject(jo["renditions"].ToString(), renditions);
+                result.renditions = renditions;
             }
             catch
             {
